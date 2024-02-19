79447b838d3f513ce63a3ce69bfaf1842c7c352d
ansaralyh <ansaralyh@gmail.com>
2024-02-19 22:54:31 +0500

    updated

--- backend/models/roomModel.js
+++ backend/models/roomModel.js
@@ -1,23 +1,25 @@
 const mongoose = require('mongoose');
 const roomSchema = new mongoose.Schema({
-    room_category:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Category"
-    },
-    room_number:{
-        type:Number,
-    },
-    image:{
-        type:String,
-    },
-    hotel_id:{
+    room_category: {
         type: mongoose.Schema.Types.ObjectId,
-        ref:"Hotel"
+        ref: "Category"
     },
-    isReserved:{
-        type:Number, // 0 for available and 1 for reserved
-        default:0
+    room_number: {
+        type: Number,
+    },
+    images: [
+        {
+            type: String
+        }
+    ],
+    hotel_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Hotel"
+    },
+    isReserved: {
+        type: Number, // 0 for available and 1 for reserved
+        default: 0
     }
 });
 
-module.exports = mongoose.model('Room',roomSchema);
+module.exports = mongoose.model('Room', roomSchema);
