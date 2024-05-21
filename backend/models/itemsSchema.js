b892b47d5d270a650db6983c48cbcf5936cb37c7
Measum Abbas <maisumabbas.charbu@gmail.com>
2024-05-22 01:49:27 +0500

    customers-edited

--- backend/models/itemsSchema.js
+++ backend/models/itemsSchema.js
@@ -1,29 +1,29 @@
-const mongoose = require('mongoose');
-const itemsSchema = new mongoose.Schema({
-    hotel_id:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Hotel"
+const mongoose = require("mongoose");
+const itemsSchema = new mongoose.Schema(
+  {
+    hotel_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hotel",
     },
-    branch_id:{
-
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Branch"
+    branch_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Branch",
     },
-    name:{
-        type:String
+    name: {
+      type: String,
     },
-    cost:{
-        type:Number
-    }
+    cost: {
+      type: Number,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+  }
+);
 
-},
-    {
-        timestamps:true,
-        toJSON:{virtuals:true}
-    });
+itemsSchema.virtual("id").get(function () {
+  return this._id;
+});
 
-    itemsSchema.virtual('id').get(function(){
-        return this._id
-    })
-
-    module.exports = mongoose.model('Items',itemsSchema)
+module.exports = mongoose.model("Items", itemsSchema);
