6e7917b188557568d08854e915eb5ed451ae8fb9
ansaralyh <ansaralyh@gmail.com>
2024-06-28 16:34:28 +0500

    expenses-backend

--- backend/models/expenseSchema.js
+++ backend/models/expenseSchema.js
@@ -16,7 +16,7 @@ const expenseSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "Lookup"
     },
-    receipt:{
+    receipt: {
         type: String
     }
 }, {
